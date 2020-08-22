d3a9a1171aa5e599d80b5f7c2c622e66399fb298
Mathias Lorenz <lorenz.mathias@gmail.com>
2020-08-22 14:00:53 +0200

    Added infrastructure project and initial db context.

---
+++ src/Infrastructure/Infrastructure/Persistence/MiniURLDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniURL.Domain.Entities;
+using System.Reflection;
+
+namespace Infrastructure.Persistence
+{
+    public class MiniURLDbContext : DbContext
+    {
+        public MiniURLDbContext(DbContextOptions<MiniURLDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
