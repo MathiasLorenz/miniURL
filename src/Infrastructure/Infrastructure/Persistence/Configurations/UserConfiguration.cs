fca82b9bd4b3066923e5ddf72f6a91917d3f4bf9
Mathias Lorenz <lorenz.mathias@gmail.com>
2020-08-22 14:06:17 +0200

    Initial user configuration.

---
+++ src/Infrastructure/Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MiniURL.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasAlternateKey(x => x.Email);
+
+            builder.Property(x => x.FirstName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(x => x.LastName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            // Todo: Use FluentValidations to validate this on creation.
+            builder.Property(x => x.Email)
+                .IsRequired()
+                .HasMaxLength(100);
+        }
+    }
+}
