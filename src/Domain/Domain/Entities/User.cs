a3a1a6419b4de1a9a5faf45f0dcf04779bfce12a
Mathias Lorenz <lorenz.mathias@gmail.com>
2020-08-22 13:51:33 +0200

    Added user entity. Will be removed later on (much later) to be replaced by IdentityServer4 as IDP.

---
+++ src/Domain/Domain/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniURL.Domain.Entities
+{
+    // Todo: Make AuditableEntity or something similar to handle createdDate etc.
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+    }
+}
