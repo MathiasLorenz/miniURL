cbdf4531a1e20c4fcd81e76eec30ec23d6a15ab7
Mathias Lorenz <lorenz.mathias@gmail.com>
2020-08-23 18:08:57 +0200

    Added MediatR package.

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 using MiniURL.Application.Common;
 using MiniURL.Application.Common.Interfaces;
+using System.Reflection;
 
 namespace MiniURL.Application
 {
@@ -9,6 +11,8 @@ namespace MiniURL.Application
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddScoped<IRNGCrypto, RNGCrypto>();
+
+            services.AddMediatR(Assembly.GetExecutingAssembly());
 
             return services;
         }
