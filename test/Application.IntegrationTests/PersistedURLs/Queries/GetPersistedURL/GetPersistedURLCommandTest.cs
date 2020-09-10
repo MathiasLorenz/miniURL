6d18b3873bcbe5a5beb12b3c1e8ecbbae4a6792e
MathiasLorenz <lorenz.mathias@gmail.com>
2020-09-10 15:45:06 +0200

    Renamed a test and made better.

--- test/Application.IntegrationTests/PersistedURLs/Queries/GetPersistedURL/GetPersistedURLCommandTest.cs
+++ test/Application.IntegrationTests/PersistedURLs/Queries/GetPersistedURL/GetPersistedURLCommandTest.cs
@@ -1,15 +1,15 @@
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MiniURL.Application.Common.Exceptions;
 using MiniURL.Application.PersistedURLs.Queries.GetPersistedURL;
+using MiniURL.Domain.Entities;
 using MiniURL.Infrastructure.Persistence;
 using Shouldly;
 
 namespace MiniURL.Application.IntegrationTests.PersistedURLs.Queries.GetPersistedURL
 {
     [TestClass]
-    public class CreatePersistedURLCommandTest : BaseTest
+    public class GetPersistedURLCommandTest : BaseTest
     {
         [TestMethod]
         public async Task Handle_RequestUnknownShortURL_ThrowsException()
@@ -30,19 +30,29 @@ namespace MiniURL.Application.IntegrationTests.PersistedURLs.Queries.GetPersiste
         [TestMethod]
         public async Task Handle_RequestKnownShortURL_ReturnsCorrectly()
         {
+            var originalURL = "https://github.com/MathiasLorenz";
             var shortURL = "iodfetry";
-            await SeedDb(DbOptions, DateTimeService);
+
+            using (var ctx = new MiniURLDbContext(DbOptions, DateTimeService))
+            {
+                ctx.PersistedURLs.Add(new PersistedURL
+                {
+                    URL = originalURL,
+                    ShortURL = shortURL,
+                });
+                await ctx.SaveChangesAsync();
+            }
 
             using (var ctx = new MiniURLDbContext(DbOptions, DateTimeService))
             {
                 var handler = new GetPersistedURLQueryHandler(ctx);
+                var query = new GetPersistedURLQuery { ShortURL = shortURL };
                 
-                var result = await handler.Handle(new GetPersistedURLQuery { ShortURL = shortURL },
-                                                  new System.Threading.CancellationToken());
+                var result = await handler.Handle(query, new System.Threading.CancellationToken());
 
                 result.ShouldNotBeNull();
                 result.ShouldBeOfType<PersistedURLVm>();
-                result.URL.ShouldBe("https://github.com/MathiasLorenz");
+                result.URL.ShouldBe(originalURL);
                 result.ShortURL.ShouldBe(shortURL);
             }
         }
